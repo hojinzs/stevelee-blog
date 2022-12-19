b20b2d3eec28746f6a19cdd26422b53ac721cdb0
stevelee <hojinzs@gmail.com>
2022-12-20 01:42:07 +0900

    auth

---
+++ src/libs/amplify.ts
@@ -0,0 +1,36 @@
+import {Amplify} from "aws-amplify";
+import awsConfig from "../aws-exports"
+export function amplifyConfig() {
+    if(typeof window !== 'undefined') {
+        const isLocalhost = Boolean(
+            window.location.hostname === "localhost" ||
+            // [::1] is the IPv6 localhost address.
+            window.location.hostname === "[::1]" ||
+            // 127.0.0.1/8 is considered localhost for IPv4.
+            window.location.hostname.match(
+                /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+            )
+        );
+
+        const [
+            localRedirectSignIn,
+            productionRedirectSignIn,
+        ] = awsConfig.oauth.redirectSignIn.split(",");
+
+        const [
+            localRedirectSignOut,
+            productionRedirectSignOut,
+        ] = awsConfig.oauth.redirectSignOut.split(",");
+
+        const updatedAwsConfig = {
+            ...awsConfig,
+            oauth: {
+                ...awsConfig.oauth,
+                redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+                redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+            }
+        }
+
+        Amplify.configure(updatedAwsConfig)
+    }
+}
